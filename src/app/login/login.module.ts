a895e900b8a2d109ce5227d35a0da393a4328665
miltondisimone <miltondisimone@gmail.com>
2020-01-02 10:32:25 -0300

    Added login and register templates

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [LoginComponent, RegisterComponent]
 })
